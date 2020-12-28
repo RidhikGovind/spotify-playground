d20cc4c226dbf0df7c5b2975534858232b57660c
RidhikGovind <ridzman2019@gmail.com>
2020-12-29 00:54:03 +0530

    minor change

--- client/src/Login/Login.js
+++ client/src/Login/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './Login.css'
+import "./Login.css";
 
 export default function Login() {
   const LOGIN_URI =
@@ -7,11 +7,10 @@ export default function Login() {
       ? "http://localhost:8888/login"
       : "https://spotifyplayground.herokuapp.com/login";
 
-  return(
-    <div className="loginSection"> 
-    <h2>Spotify Playground</h2>
-    <a href={LOGIN_URI} >Login</a>
+  return (
+    <div className="loginSection">
+      <h2>Spotify Playground</h2>
+      <a href={LOGIN_URI}>Login</a>
     </div>
-  )
-   
+  );
 }
